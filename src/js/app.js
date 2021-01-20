c83aa2963906c0a8997982c3b56af44bc5678bdd
RobertoKlann <robertoklann17@gmail.com>
2021-01-19 21:39:34 -0300

    Iniciando projeto TECHNO

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+const techno = new Vue({
+    el: '#app',
+    data: {
+        produtos: []
+    },
+    methods: {
+        fetchProdutos() {
+            debugger;
+            fetch('../../../api/produtos.json').then(r => r.json()).then(r => {
+                this.produtos = r;
+            });
+        }
+    },
+    created() {
+        this.fetchProdutos();
+    }
+});
