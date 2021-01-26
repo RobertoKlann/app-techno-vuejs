b9e1e9c8ec1569d14e4a562134b65fbd0643823c
RobertoKlann <robertoklann17@gmail.com>
2021-01-25 21:26:39 -0300

    Novas funcionalidades

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,37 @@
 const techno = new Vue({
     el: '#app',
     data: {
-        produtos: []
+        produtos: [],
+        produto: null,
+        carrinhoTotal: 0
+    },
+
+    filters: {
+        formaterPreco(valor) {
+            return valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+        }
     },
     methods: {
         fetchProdutos() {
-            debugger;
             fetch('../../../api/produtos.json').then(r => r.json()).then(r => {
                 this.produtos = r;
             });
+        },
+        fetchProduto(id) {
+            fetch(`../../../api/produtos/${id}/dados.json`).then(r => r.json()).then(r => {
+                this.produto = r;
+            });
+        },
+        abrirModal(id) {
+            this.fetchProduto(id);
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        },
+        fecharModal({target, currentTarget}) {
+            if(target == currentTarget)
+                this.produto = false;
         }
     },
     created() {
